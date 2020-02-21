35d249418c0be3e12927dc653feca82274cd021c
Alexander <alex.sviatetskyi@gmail.com>
2020-02-21 16:25:51 +0200

    Add logger

---
+++ EndlessEngine/System/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace EndlessEngine.System
+{
+    public class Log
+    {
+        public static Logger Instance { get; }
+
+        static Log()
+        {
+            var config = new LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new FileTarget("logfile") { FileName = "log.txt" };
+            var logconsole = new ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            LogManager.Configuration = config;
+
+            Instance = LogManager.GetCurrentClassLogger();
+        }
+    }
+}

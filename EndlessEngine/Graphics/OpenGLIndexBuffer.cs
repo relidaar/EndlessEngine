784bcc9d9a8f9e135892c7dd2013098ecead4db7
Alexander <alex.sviatetskyi@gmail.com>
2020-02-29 17:43:24 +0200

    Implement OpenGL buffers

---
+++ EndlessEngine/Graphics/OpenGLIndexBuffer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EndlessEngine.Graphics.Interfaces;
+using OpenGL;
+
+namespace EndlessEngine.Graphics
+{
+    public class OpenGLIndexBuffer : IIndexBuffer, IDisposable
+    {
+        public int[] Indices { get; }
+        private readonly uint _id;
+
+        public OpenGLIndexBuffer(int[] indices)
+        {
+            Indices = indices;
+            var size = (uint)(indices.Length * sizeof(int));
+
+            _id = Gl.GenBuffer();
+            Gl.BindBuffer(BufferTarget.ElementArrayBuffer, _id);
+            Gl.BufferData(BufferTarget.ElementArrayBuffer, size, indices, BufferUsage.StaticDraw);
+        }
+
+        public void Bind() =>
+            Gl.BindBuffer(BufferTarget.ElementArrayBuffer, _id);
+
+        public void Unbind() =>
+            Gl.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+
+        public void Dispose() =>
+            Gl.DeleteBuffers(_id);
+    }
+}
---
+++ EndlessEngine/Graphics/OpenGLVertexBuffer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EndlessEngine.Graphics.Interfaces;
+using OpenGL;
+
+namespace EndlessEngine.Graphics
+{
+    public class OpenGLVertexBuffer : IVertexBuffer, IDisposable
+    {
+        public float[] Vertices { get; }
+        public IBufferLayout Layout { get; set; }
+        
+        private readonly uint _id;
+
+        public OpenGLVertexBuffer(float[] vertices)
+        {
+            Vertices = vertices;
+            var size = (uint)(vertices.Length * sizeof(float));
+
+            _id = Gl.GenBuffer();
+            Gl.BindBuffer(BufferTarget.ArrayBuffer, _id);
+            Gl.BufferData(BufferTarget.ArrayBuffer, size, vertices, BufferUsage.StaticDraw);
+        }
+
+        public void Bind() =>
+            Gl.BindBuffer(BufferTarget.ArrayBuffer, _id);
+
+        public void Unbind() =>
+            Gl.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+        public void Dispose() =>
+            Gl.DeleteBuffers(_id);
+    }
+}

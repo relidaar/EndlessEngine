6709c3adc9053e61d02010fb0e0ba141ec209d83
Alexander <alex.sviatetskyi@gmail.com>
2020-02-29 17:43:51 +0200

    Implement buffer layout

---
+++ EndlessEngine/Graphics/OpenGLBufferLayout.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EndlessEngine.Graphics.Interfaces;
+
+namespace EndlessEngine.Graphics
+{
+    public class OpenGLBufferLayout : IBufferLayout
+    {
+        public IEnumerable<BufferElement> Elements => _elements;
+        public int Stride { get; private set; }
+
+        private readonly List<BufferElement> _elements;
+
+        public OpenGLBufferLayout(params BufferElement[] elements)
+        {
+            _elements = new List<BufferElement>(elements);
+            CalculateOffsetAndStride();
+        }
+
+        private void CalculateOffsetAndStride()
+        {
+            int offset = 0;
+            Stride = 0;
+
+            for (var i = 0; i < _elements.Count; i++)
+            {
+                var element = _elements[i];
+                element.Offset = offset;
+                _elements[i] = element;
+
+                offset += element.Size;
+                Stride += element.Size;
+            }
+        }
+    }
+}

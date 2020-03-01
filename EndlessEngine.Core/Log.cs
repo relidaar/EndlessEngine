a0cad6cff639dfcec6304996930b3dd4ef670a2e
Alexander <alex.sviatetskyi@gmail.com>
2020-03-01 18:57:20 +0200

    Add Core project

---
+++ EndlessEngine.Core/Log.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace EndlessEngine.Core
+{
+    public class Log
+    {
+        public static Logger Instance { get; }
+
+        static Log()
+        {
+            var config = new LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new FileTarget("logfile") { FileName = "log.txt" };
+            var logconsole = new ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            LogManager.Configuration = config;
+
+            Instance = LogManager.GetCurrentClassLogger();
+        }
+    }
+}

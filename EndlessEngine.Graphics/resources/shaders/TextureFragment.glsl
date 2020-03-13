99741afc4e0e7049a72823efaf13bea6e2447ad3
Alexander <alex.sviatetskyi@gmail.com>
2020-03-13 10:41:53 +0200

    Create shaders for textures

---
+++ EndlessEngine.Graphics/resources/shaders/TextureFragment.glsl
@@ -0,0 +1,9 @@
+﻿#version 330 core
+
+uniform vec4 uColor;
+
+layout(location = 0) out vec4 color;
+
+void main() {
+    color = uColor;
+}
---
+++ EndlessEngine.Graphics/resources/shaders/TextureVertex.glsl
@@ -0,0 +1,9 @@
+﻿#version 330 core
+
+layout(location = 0) in vec4 aPosition;
+
+uniform mat4 uTransform;
+
+void main() {
+    gl_Position = uTransform * aPosition;
+}

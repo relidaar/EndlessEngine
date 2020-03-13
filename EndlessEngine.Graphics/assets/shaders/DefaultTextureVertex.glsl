b0a32bfb86f899bf337908ef334103f71c549ccc
Alexander <alex.sviatetskyi@gmail.com>
2020-03-13 13:59:43 +0200

    Update default shaders

--- EndlessEngine.Graphics/assets/shaders/DefaultTextureVertex.glsl
+++ EndlessEngine.Graphics/assets/shaders/DefaultTextureVertex.glsl
@@ -1,9 +1,13 @@
 ﻿#version 330 core
 
 layout(location = 0) in vec4 aPosition;
+layout(location = 1) in vec2 aTextureCoordinates;
 
 uniform mat4 uTransform;
 
+out vec2 vTextureCoordinates;
+
 void main() {
     gl_Position = uTransform * aPosition;
+    vTextureCoordinates = aTextureCoordinates;
 }
--- EndlessEngine.Graphics/assets/shaders/TextureFragment.glsl
+++
@@ -1,9 +0,0 @@
-﻿#version 330 core
-
-uniform vec4 uColor;
-
-layout(location = 0) out vec4 color;
-
-void main() {
-    color = uColor;
-}

9ad9bd51eaf00020ab07aa180add593c7c78fc95
Alexander <alex.sviatetskyi@gmail.com>
2020-03-15 11:50:00 +0200

    Join shaders for texture and color

--- EndlessEngine.Graphics/assets/shaders/DefaultColorVertex.glsl
+++
@@ -1,9 +0,0 @@
-﻿#version 330 core
-
-layout(location = 0) in vec4 aPosition;
-
-uniform mat4 uTransform;
-
-void main() {
-    gl_Position = uTransform * aPosition;
-}
--- EndlessEngine.Graphics/assets/shaders/DefaultFragment.glsl
+++ EndlessEngine.Graphics/assets/shaders/DefaultFragment.glsl
@@ -3,9 +3,10 @@
 in vec2 vTextureCoordinates;
 
 uniform sampler2D uTexture;
+uniform vec4 uColor;
 
 layout(location = 0) out vec4 color;
 
 void main() {
-    color = texture(uTexture, vTextureCoordinates);
+    color = texture(uTexture, vTextureCoordinates) * uColor;
 }

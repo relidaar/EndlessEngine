03ce8c5b287b57dc0ae7d3feba23d732af27bd25
Alexander <alex.sviatetskyi@gmail.com>
2020-03-19 14:07:27 +0200

    Implement Color tests for operators

--- EndlessEngine.Graphics.Test/DataTypes/ColorTests.cs
+++ EndlessEngine.Graphics.Test/DataTypes/ColorTests.cs
@@ -140,5 +140,98 @@ namespace EndlessEngine.Graphics.Test.DataTypes
         }
 
         #endregion
+   
+        #region Operators
+
+        [Theory]
+        [InlineData(-1, -2, -3, -4)]
+        [InlineData(1, 2, 3, 4)]
+        [InlineData(256, 257, 258, 259)]
+        public void AddColorOperator(params int[] data)
+        {
+            var color1 = new Color(0);
+            var color2 = new Color(data[0], data[1], data[2], data[3]);
+
+            var result = (color1 + color2).ValuesArray;
+            var expected = data.Select(x => Color.Mod(x, 256));
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void AddValueOperator(int value)
+        {
+            var color = new Color(0);
+
+            var result = (color + value).ValuesArray;
+            var expected = new int[4];
+            for (var i = 0; i < expected.Length; i++) expected[i] = Color.Mod(value, 256);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(-1, -2, -3, -4)]
+        [InlineData(1, 2, 3, 4)]
+        [InlineData(256, 257, 258, 259)]
+        public void SubtractColorOperator(params int[] data)
+        {
+            var color1 = new Color(0);
+            var color2 = new Color(data[0], data[1], data[2], data[3]);
+
+            var result = (color1 - color2).ValuesArray;
+            var expected = data.Select(x => Color.Mod(0 - x, 256));
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void SubtractValueOperator(int value)
+        {
+            var color = new Color(1);
+
+            var result = (color - value).ValuesArray;
+            var expected = new int[4];
+            for (var i = 0; i < expected.Length; i++) expected[i] = Color.Mod(1 - value, 256);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void MultiplyByValueOperator(int value)
+        {
+            var color = new Color(1);
+
+            var result = (color * value).ValuesArray;
+            var expected = new int[4];
+            for (var i = 0; i < expected.Length; i++) expected[i] = Color.Mod(value, 256);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(1)]
+        public void DivideByValueOperator(int value)
+        {
+            var color = new Color(1);
+
+            var result = (color / value).ValuesArray;
+            var expected = new int[4];
+            for (var i = 0; i < expected.Length; i++) expected[i] = Color.Mod(1 / value, 256);
+
+            Assert.Equal(expected, result);
+        }
+
+        #endregion
     }
 }

2a792b4e8813ed5acb8366ef6f2328ff1e2ab28f
Alexander <alex.sviatetskyi@gmail.com>
2020-04-17 18:53:53 +0300

    Add key events

---
+++ EndlessEngine.Graphics/Events/KeyEvent.cs
@@ -0,0 +1,42 @@
+﻿using EndlessEngine.Graphics.Interfaces;
+
+namespace EndlessEngine.Graphics.Events
+{
+    public class KeyPressedEvent : IKeyEvent
+    {
+        public EventType Type { get; }
+        public Key Key { get; }
+        public int RepeatCount { get; }
+
+        public KeyPressedEvent(Key key, int repeatCount)
+        {
+            Type = EventType.KeyPressed;
+            Key = key;
+            RepeatCount = repeatCount;
+        }
+    }
+
+    public class KeyReleasedEvent : IKeyEvent
+    {
+        public EventType Type { get; }
+        public Key Key { get; }
+
+        public KeyReleasedEvent(Key key)
+        {
+            Type = EventType.KeyReleased;
+            Key = key;
+        }
+    }
+    
+    public class KeyTypedEvent : IKeyEvent
+    {
+        public EventType Type { get; }
+        public Key Key { get; }
+        
+        public KeyTypedEvent(Key key)
+        {
+            Type = EventType.KeyTyped;
+            Key = key;
+        }
+    }
+}

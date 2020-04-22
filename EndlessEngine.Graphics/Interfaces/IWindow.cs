b0cec277eda2370174736341271fb721de50dfee
Alexander <alex.sviatetskyi@gmail.com>
2020-04-22 19:01:20 +0300

    Add custom events

--- EndlessEngine.Graphics/Interfaces/IWindow.cs
+++ EndlessEngine.Graphics/Interfaces/IWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using EndlessEngine.Graphics.Events;
 
 namespace EndlessEngine.Graphics.Interfaces
 {
@@ -11,6 +12,24 @@ namespace EndlessEngine.Graphics.Interfaces
         void Display();
         void Close();
 
+        #region Events
+        
         event EventHandler<IEvent> OnEvent;
+
+        event EventHandler<WindowCloseEvent> OnWindowClose;
+        event EventHandler<WindowMovedEvent> OnWindowMoved;
+        event EventHandler<WindowResizeEvent> OnWindowResize;
+        event EventHandler<WindowFocusEvent> OnWindowFocus;
+
+        event EventHandler<KeyPressedEvent> OnKeyPressed;
+        event EventHandler<KeyReleasedEvent> OnKeyReleased;
+        event EventHandler<KeyTypedEvent> OnKeyTyped;
+        
+        event EventHandler<MouseButtonPressedEvent> OnMouseButtonPressed;
+        event EventHandler<MouseButtonReleasedEvent> OnMouseButtonReleased;
+        event EventHandler<MouseScrolledEvent> OnMouseScrolled;
+        event EventHandler<MouseMovedEvent> OnMouseMoved;
+
+        #endregion
     }
 }

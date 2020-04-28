42f3373da3182a8f58cbb556c1a277c32b0f2129
Alexander <alex.sviatetskyi@gmail.com>
2020-04-28 16:26:53 +0300

    Create tests for OpenGL window

---
+++ EndlessEngine.Graphics.Test/OpenGLTests/OpenGLWindowTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EndlessEngine.Graphics.Interfaces;
+using EndlessEngine.Graphics.OpenGL;
+using Xunit;
+
+namespace EndlessEngine.Graphics.Test.OpenGLTests
+{
+    public class OpenGLWindowTests
+    {
+        [Fact]
+        public void CreateWithNegativeValues()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new OpenGLWindow(-1, -1, "Test", new GraphicsSettings()));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void CreateWithIncorrectString(string title)
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new OpenGLWindow(0, 0, title, new GraphicsSettings()));
+        }
+    }
+}

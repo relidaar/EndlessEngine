fb18b2ffd0a3e62e1188cd6b781b8cff0f0d387a
Alexander <alex.sviatetskyi@gmail.com>
2020-04-28 16:11:01 +0300

    Create tests for OpenGL texture

---
+++ EndlessEngine.Graphics.Test/OpenGLTests/OpenGLTextureTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EndlessEngine.Graphics.OpenGL;
+using Xunit;
+
+namespace EndlessEngine.Graphics.Test.OpenGLTests
+{
+    public class OpenGLTextureTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void CreateWithIncorrectPath(string path)
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLTexture(path, TextureData.Default));
+        }
+
+        [Fact]
+        public void CreateWithNullReferenceObject()
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLTexture(0, 0, null, TextureData.Default));
+        }
+
+        [Fact]
+        public void CreateWithNegativeValues()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new OpenGLTexture(-1, -1, new float[1], TextureData.Default));
+        }
+    }
+}

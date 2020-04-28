3e24eb34ccd25acd91e7ff30b6a69f9a71e3d677
Alexander <alex.sviatetskyi@gmail.com>
2020-04-28 14:13:51 +0300

    Create ctor tests for OpenGL shader

---
+++ EndlessEngine.Graphics.Test/OpenGLTests/OpenGLShaderTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using EndlessEngine.Graphics.OpenGL;
+using Xunit;
+
+namespace EndlessEngine.Graphics.Test.OpenGLTests
+{
+    public class OpenGLShaderTests
+    {
+        [Fact]
+        public void CreateWithNullReferenceParameters()
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader(null, "", ""));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("", null, ""));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("", "", null));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader(null, null, ""));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("", null, null));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader(null, "", null));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader(null, null, null));
+        }
+
+        [Fact]
+        public void CreateWithEmptyStringParameters()
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("", "path1", "path2"));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("test", "", "path2"));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("test", "path1", ""));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("test", "", ""));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("", "path1", ""));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("", "", "path2"));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new OpenGLShader("", "", ""));
+        }
+
+        [Fact]
+        public void CreateWithNonexistentFilepath()
+        {
+            Assert.Throws<FileNotFoundException>(() => 
+                new OpenGLShader("test", "path1", "path2"));
+        }
+    }
+}

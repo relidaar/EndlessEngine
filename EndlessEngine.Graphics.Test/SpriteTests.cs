23052ac4cc75557d5db07c70c62c0f55c8d6a158
Alexander <alex.sviatetskyi@gmail.com>
2020-04-29 15:28:00 +0300

    Create tests for sprite

---
+++ EndlessEngine.Graphics.Test/SpriteTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EndlessEngine.Math;
+using Xunit;
+
+namespace EndlessEngine.Graphics.Test
+{
+    public class SpriteTests
+    {
+        [Fact]
+        public void CreateWithNullReferenceTexture()
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+                new Sprite(null, 0, 0, 0));
+            
+            Assert.Throws<ArgumentNullException>(() => 
+                new Sprite(null, 0, 0, 0, 0));
+            
+            Assert.Throws<ArgumentNullException>(() =>
+                new Sprite(null, new Vector2(0), new Vector2(0)));
+        }
+
+        [Fact]
+        public void CreateWithNegativeSize()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => 
+                new Sprite(null, 0, 0, -1));
+            
+            Assert.Throws<ArgumentOutOfRangeException>(() => 
+                new Sprite(null, 0, 0, -1, -1));
+            
+            Assert.Throws<ArgumentOutOfRangeException>(() => 
+                new Sprite(null, new Vector2(0), new Vector2(-1)));
+        }
+    }
+}

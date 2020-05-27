11ed22d93e7dd51e2284a7518edce97f7287349c
Alexander <alex.sviatetskyi@gmail.com>
2020-05-27 18:11:32 +0300

    Disable console in Release

--- EndlessEngine.Sandbox/Program.cs
+++ EndlessEngine.Sandbox/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace EndlessEngine.Sandbox
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EndlessEngine.Sandbox
 {
     public class Program
     {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        
         public static void Main(string[] args)
         {
+#if !DEBUG
+            IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(h, 0);
+#endif
             var game = new Game(800, 600, "Sample Game");
             game.Run();
         }
